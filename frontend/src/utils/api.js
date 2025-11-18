aacf0d6ba43445615a9efbf4b7799739b9ffe139
MaGNeT <125195661+its-m4gn3t@users.noreply.github.com>
2025-11-19 00:33:06 +0545

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import useAuthStore from '@/store/authStore';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: 'https://skillsathi.onrender.com/api',
   withCredentials: true,  
 });
 
